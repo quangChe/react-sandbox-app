1114286dcda324930638e8d70f0b9f73e7a824ab
quangChe <quang.n.che@gmail.com>
2020-08-11 19:32:20 -0700

    BuildControls Completed
    Completed basic functionality and rendering of BuildControls in the
    BurgerBuilder component.

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import './BuildControls.scss';
+import ControlBox from './ControlBox/ControlBox';
+
+const controlItems = [
+  {label: 'Salad', type: 'salad'},
+  {label: 'Meat', type: 'meat'},
+  {label: 'Cheese', type: 'cheese'},
+  {label: 'Bacon', type: 'bacon'}
+]
+
 const BuildControls = props => {
   return (
-    <div>
-      
+    <div className="BuildControls">
+      { 
+        controlItems.map((ctrl, i) => 
+          <ControlBox key={ctrl.label + i} label={ctrl.label}></ControlBox>
+        )
+      }
     </div>
   )
 };  
--- src/components/Burger/BuildControls/ControlBox/ControlBox.js
+++ src/components/Burger/BuildControls/ControlBox/ControlBox.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import './ControlBox.scss';
+
 const ControlBox = props => {
   return (
-    <div>
+    <div className="ControlBox">
       <div className="Label">{props.label}</div>
       <button className="Less">Less</button>
       <button className="More">More</button>
