0924329fdd21b519415322b75dd6d3ed24eb9464
quangChe <quang.n.che@gmail.com>
2020-08-13 13:05:24 -0700

    Completed toggling funcitonality of SideDrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './Layout.scss';
 import Aux from '../../hoc/Aux';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const Layout = (props) => (
-  <Aux>
-    <Toolbar/>
-    <SideDrawer/>
-    <main className="MainContent">
-      {props.children}
-    </main>
-  </Aux>
-);
+class Layout extends Component {
+
+  state = {
+    showSideDrawer: false,
+  }
+
+  sideDrawerToggle = () => {
+    this.setState({showSideDrawer: !this.state.showSideDrawer})
+  }
+
+  render() {
+    return (
+      <Aux>
+        <Toolbar
+          toggleSideBar={this.sideDrawerToggle}/>
+        <SideDrawer 
+          opened={this.state.showSideDrawer} 
+          toggle={this.sideDrawerToggle}/>
+        <main className="MainContent">
+          {this.props.children}
+        </main>
+      </Aux>
+    )
+  }
+}
+  
+
 
 export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,20 +1,36 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import './SideDrawer.scss';
 import Logo from '../../UI/Logo/Logo';
 import NavItemsList from '../NavItemsList/NavItemsList';
+import Backdrop from "../../UI/Backdrop/Backdrop";
 
 const SideDrawer = props => {
+  const toggleClass = [
+    "SideDrawer", 
+    props.opened ? "Open" : "Close"
+  ].join(' ');
+
+  console.log(toggleClass);
   return (
-    <div className="SideDrawer">
-      <div className="LogoContainer">
-        <Logo/>
+    <>
+      <Backdrop show={props.opened} hide={props.toggle}/>
+      <div className={toggleClass}>
+        <div className="LogoContainer">
+          <Logo/>
+        </div>
+        <nav>
+          <NavItemsList/>
+        </nav>
       </div>
-      <nav>
-        <NavItemsList/>
-      </nav>
-    </div>
+    </>
   )
 };
+
+SideDrawer.propTypes = {
+  opened: PropTypes.bool.isRequired,
+  toggle: PropTypes.func.isRequired
+}
 
 export default SideDrawer;
