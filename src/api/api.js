d5d8aef03a2802942613e411277622bc4669e58f
quangChe <quang.n.che@gmail.com>
2020-08-14 16:51:52 -0700

    Updated API URL

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
-console.log(process.env.REACT_APP_API_URL);
-
 const burgerBuilderAPI = axios.create({
-  baseURL: process.env.API_URL
+  baseURL: process.env.REACT_APP_API_URL
 })
 
 export default burgerBuilderAPI;
